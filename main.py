8bfcff4c6366c3045ed2689db313fd52308980ff
Robin Tainton <robintainton0509@gmail.com>
2021-12-25 19:18:07 +0200

    implementing saving data and error handling

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from tkinter import *
+import json
+
+FILE = "password_data.json"
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save_data():
+    website = website_input.get()
+    username = username_input.get()
+    password = password_input.get()
+    new_data = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
+    try:
+        with open(FILE, "r") as file_data:
+            data = json.load(file_data)
+    except FileNotFoundError:
+        with open(FILE, "w") as file_data:
+            json.dump(new_data, file_data, indent=4)
+    else:
+        data.update(new_data)
+        with open(FILE, "w") as file_data:
+            json.dump(data, file_data, indent=4)
+    finally:
+        website_input.delete(0, END)
+        password_input.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -37,7 +64,7 @@ search_button.grid(row=1, column=2, sticky="we")
 generate_password = Button(text="Generate Password")
 generate_password.grid(row=3, column=2, sticky="we")
 
-add_button = Button(text="Add")
+add_button = Button(text="Add", command=save_data)
 add_button.grid(row=4, column=1, columnspan=2, sticky="we")
 
 window.mainloop()
